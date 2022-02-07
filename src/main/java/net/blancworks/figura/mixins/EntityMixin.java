ee1b7f6068c8a1fab21a371b8e8dc1980585f9b8
Fran <franngoesa@gmail.com>
2022-02-07 13:22:32 -0300

    stuff

--- src/main/java/net/blancworks/figura/mixins/EntityMixin.java
+++ src/main/java/net/blancworks/figura/mixins/EntityMixin.java
@@ -1,24 +1,17 @@
 package net.blancworks.figura.mixins;
 
 import net.blancworks.figura.accessors.FiguraMetadataHolder;
-import net.blancworks.figura.dealer.FiguraDealer;
 import net.blancworks.figura.dealer.FiguraHouse;
 import net.blancworks.figura.entity.FiguraEntityMetadata;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.UUID;
-
 @Mixin(Entity.class)
 public abstract class EntityMixin implements FiguraMetadataHolder {
-
-    @Shadow
-    public abstract String getUuidAsString();
 
     @Unique
     private FiguraEntityMetadata figuraMetadata;
--- src/main/java/net/blancworks/figura/mixins/EntityRendererMixin.java
+++ src/main/java/net/blancworks/figura/mixins/EntityRendererMixin.java
@@ -1,19 +1,12 @@
 package net.blancworks.figura.mixins;
 
 import net.blancworks.figura.accessors.FiguraMetadataHolder;
-import net.blancworks.figura.avatar.FiguraAvatar;
-import net.blancworks.figura.dealer.FiguraHouse;
 import net.blancworks.figura.entity.FiguraEntityMetadata;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Vec3f;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
