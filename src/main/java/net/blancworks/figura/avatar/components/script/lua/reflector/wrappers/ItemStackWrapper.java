a711bc56d7c76ae6df3c6470ebfa8a15e3dce740
ENinja33 <83429328+ENinja33@users.noreply.github.com>
2022-02-11 19:51:16 -0500

    ItemStackWrapper and (WIP) BlockStateWrapper
    Hopefully even I can't have messed this up

--- src/main/java/net/blancworks/figura/avatar/components/script/lua/reflector/wrappers/ItemStackWrapper.java
+++ src/main/java/net/blancworks/figura/avatar/components/script/lua/reflector/wrappers/ItemStackWrapper.java
@@ -1,6 +1,7 @@
 package net.blancworks.figura.avatar.components.script.lua.reflector.wrappers;
 
 import net.blancworks.figura.avatar.components.script.lua.reflector.LuaWhitelist;
+import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemStack;
 
 public class ItemStackWrapper extends ObjectWrapper<ItemStack> {
@@ -16,4 +17,60 @@ public class ItemStackWrapper extends ObjectWrapper<ItemStack> {
     public int getDamage() {
         return target.getDamage();
     }
+
+    @LuaWhitelist
+    public boolean isEmpty() {
+        return target.isEmpty();
+    }
+
+    @LuaWhitelist
+    public float getMiningSpeedMultiplier(BlockState state) {
+        return target.getMiningSpeedMultiplier(state);
+    }
+
+    @LuaWhitelist
+    public boolean isStackable() {
+        return target.isStackable();
+    }
+
+    @LuaWhitelist
+    public boolean isDamageable() {
+        return target.isDamageable();
+    }
+
+    @LuaWhitelist
+    public boolean isDamaged() {
+        return target.isDamaged();
+    }
+
+    @LuaWhitelist
+    public boolean isSuitableFor(BlockState state) {
+        return target.isSuitableFor(state);
+    }
+
+    @LuaWhitelist
+    public String getName() {
+        return target.getName().asString();
+    }
+
+    @LuaWhitelist
+    public boolean hasGlint() {
+        return target.hasGlint();
+    }
+
+    @LuaWhitelist
+    public String getRarity() {
+        return target.getRarity().name();
+    }
+
+    @LuaWhitelist
+    public int getRepairCost() {
+        return target.getRepairCost();
+    }
+
+    @LuaWhitelist
+    public boolean isFood() {
+        return target.isFood();
+    }
+
 }
