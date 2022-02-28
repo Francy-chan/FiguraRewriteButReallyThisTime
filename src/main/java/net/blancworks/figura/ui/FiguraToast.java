0bc9ef931283086da3809c39e4340e1c2b9aed98
Fran <franngoesa@gmail.com>
2022-02-28 10:52:36 -0300

    toasts 🍞

--- src/main/java/net/blancworks/figura/ui/FiguraToast.java
+++ src/main/java/net/blancworks/figura/ui/FiguraToast.java
@@ -18,17 +18,15 @@ public class FiguraToast implements Toast {
 
     private final Text title;
     private final Text message;
-    private final boolean cheese;
+    private final ToastType type;
 
     private long startTime;
     private boolean justUpdated;
 
-    private static final Identifier TEXTURE = new Identifier("figura", "textures/gui/toast.png");
-
-    public FiguraToast(Text title, Text message) {
-        this.cheese = FiguraMod.CHEESE_DAY || Math.random() < 0.0001;
-        this.title = title.shallowCopy().fillStyle(Style.EMPTY.withColor(cheese ? 0xF8C53A : 0x55FFFF));
+    public FiguraToast(Text title, Text message, ToastType type) {
+        this.title = title.shallowCopy().fillStyle(Style.EMPTY.withColor(type.color));
         this.message = message;
+        this.type = type;
     }
 
     @Override
@@ -40,8 +38,8 @@ public class FiguraToast implements Toast {
             this.justUpdated = false;
         }
 
-        RenderSystem.setShaderTexture(0, TEXTURE);
-        DrawableHelper.drawTexture(matrices, 0, 0, 0f, cheese ? 0 : (int) (timeDiff / 208 % 4 + 1) * 32f, getWidth(), getHeight(), 160, 160);
+        RenderSystem.setShaderTexture(0, type.texture);
+        DrawableHelper.drawTexture(matrices, 0, 0, 0f, (int) (timeDiff / 208 % type.frames + 1) * 32f, getWidth(), getHeight(), 160, 32 * type.frames);
 
         TextRenderer renderer = manager.getClient().textRenderer;
         if (this.message == null) {
@@ -59,12 +57,40 @@ public class FiguraToast implements Toast {
         sendToast(title, LiteralText.EMPTY);
     }
 
+    public static void sendToast(Object title, ToastType type) {
+        sendToast(title, LiteralText.EMPTY, type);
+    }
+
     public static void sendToast(Object title, Object message) {
+        sendToast(title, message, ToastType.DEFAULT);
+    }
+
+    public static void sendToast(Object title, Object message, ToastType type) {
         Text text = title instanceof Text t ? t : new TranslatableText(title.toString());
         Text text2 = message instanceof Text m ? m : new TranslatableText(message.toString());
 
+        if (type == ToastType.DEFAULT && (FiguraMod.CHEESE_DAY || Math.random() < 0.0001))
+            type = ToastType.CHEESE;
+
         ToastManager toasts = MinecraftClient.getInstance().getToastManager();
         toasts.clear();
-        toasts.add(new FiguraToast(text, text2));
+        toasts.add(new FiguraToast(text, text2, type));
+    }
+
+    public enum ToastType {
+        DEFAULT(new Identifier("figura", "textures/gui/toast/default.png"), 4, 0x55FFFF),
+        WARNING(new Identifier("figura", "textures/gui/toast/warning.png"), 4, 0xFFFF00),
+        ERROR(new Identifier("figura", "textures/gui/toast/error.png"), 4, 0xFF0000),
+        CHEESE(new Identifier("figura", "textures/gui/toast/cheese.png"), 1, 0xF8C53A);
+
+        private final Identifier texture;
+        private final int frames;
+        private final int color;
+
+        ToastType(Identifier texture, int frames, int color) {
+            this.texture = texture;
+            this.frames = frames;
+            this.color = color;
+        }
     }
 }
--- src/main/java/net/blancworks/figura/ui/panels/BrowserPanel.java
+++ src/main/java/net/blancworks/figura/ui/panels/BrowserPanel.java
@@ -2,8 +2,10 @@ package net.blancworks.figura.ui.panels;
 
 import net.blancworks.figura.ui.FiguraToast;
 import net.blancworks.figura.ui.helpers.UIHelper;
+import net.blancworks.figura.ui.widgets.TexturedButton;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
@@ -20,11 +22,26 @@ public class BrowserPanel extends Panel {
     @Override
     public void init() {
         FiguraToast.sendToast("not yet!", "<3");
+
+        int y = -72;
+        this.addDrawableChild(new TexturedButton(width / 2 - 30, height / 2 + (y += 24), 60, 20, new LiteralText("default toast"), button -> {
+            FiguraToast.sendToast("default", "test", FiguraToast.ToastType.DEFAULT);
+        }));
+        this.addDrawableChild(new TexturedButton(width / 2 - 30, height / 2 + (y += 24), 60, 20, new LiteralText("error toast"), button -> {
+            FiguraToast.sendToast("error", "test", FiguraToast.ToastType.ERROR);
+        }));
+        this.addDrawableChild(new TexturedButton(width / 2 - 30, height / 2 + (y += 24), 60, 20, new LiteralText("warning toast"), button -> {
+            FiguraToast.sendToast("warning", "test", FiguraToast.ToastType.WARNING);
+        }));
+        this.addDrawableChild(new TexturedButton(width / 2 - 30, height / 2 + (y + 24), 60, 20, new LiteralText("cheese toast"), button -> {
+            FiguraToast.sendToast("cheese", "test", FiguraToast.ToastType.CHEESE);
+        }));
     }
 
     @Override
     public void render(MatrixStack stack, int mouseX, int mouseY, float delta) {
         Vec2f screen = new Vec2f(MinecraftClient.getInstance().getWindow().getScaledWidth(), MinecraftClient.getInstance().getWindow().getScaledHeight());
         UIHelper.renderBackgroundTexture((int) screen.x, (int) screen.y, BACKGROUND);
+        super.render(stack, mouseX, mouseY, delta);
     }
 }
