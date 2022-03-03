b5138a72e55d307f10f98516942dea7b29b6a81b
TheOneTrueZandra <aaron.mich.mills@gmail.com>
2022-03-03 12:38:30 -0700

    Clean up imports for builtin extension

--- src/main/java/net/blancworks/figura/BuiltinFiguraExtension.java
+++ src/main/java/net/blancworks/figura/BuiltinFiguraExtension.java
@@ -10,6 +10,8 @@ import net.blancworks.figura.avatar.script.api.wrappers.item.ItemStackWrapper;
 import net.blancworks.figura.avatar.script.api.wrappers.world.BiomeWrapper;
 import net.blancworks.figura.avatar.script.api.wrappers.world.WorldWrapper;
 import net.blancworks.figura.avatar.script.api.wrappers.world.entity.LivingEntityWrapper;
+import net.blancworks.figura.avatar.script.lua.reflector.LuaWhitelist;
+import net.blancworks.figura.avatar.script.lua.reflector.wrappers.ObjectWrapper;
 import net.blancworks.figura.utils.external.FiguraExtension;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
